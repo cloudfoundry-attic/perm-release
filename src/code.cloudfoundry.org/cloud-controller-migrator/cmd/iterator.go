7d0b9c1b9bd13147c618dd1aafcdc18b0159e87a
Chris Hendrix <chendrix@pivotal.io>
2017-11-08 09:22:00 -0800

    List all organizations
    [#151615847]

--- src/code.cloudfoundry.org/cloud-controller-migrator/cmd/iterator.go
+++ src/code.cloudfoundry.org/cloud-controller-migrator/cmd/iterator.go
@@ -6,7 +6,8 @@ import (
 	"net/http"
 
 	"fmt"
-	"net/http/httputil"
+
+	"encoding/json"
 
 	"code.cloudfoundry.org/cloud-controller-migrator/cloudcontroller"
 	"code.cloudfoundry.org/cloud-controller-migrator/messages"
@@ -23,30 +24,93 @@ func IterateOverCloudControllerEntities(ctx context.Context, logger lager.Logger
 	routerLogger := logger.WithData(lager.Data{
 		"routes": rg.Routes,
 	})
-	route := cloudcontroller.Info
-	req, err := rg.CreateRequest(route, nil, nil)
+
+	var (
+		route string
+		req   *http.Request
+		res   *http.Response
+		err   error
+	)
+
+	var (
+		routeLogger lager.Logger
+	)
+
+	// /v2/info - Equivalent to Ping
+	route = cloudcontroller.Info
+	routeLogger = routerLogger.WithData(lager.Data{
+		"route": route,
+	})
+	req, err = rg.CreateRequest(route, nil, nil)
 	if err != nil {
-		routerLogger.Error(messages.FailedToCreateRequest, err, lager.Data{
-			"route": route,
-		})
+		routeLogger.Error(messages.FailedToCreateRequest, err)
 		return err
 	}
 
-	res, err := client.Do(req)
+	res, err = client.Do(req)
 	if err != nil {
-		routerLogger.Error(messages.FailedToPerformRequest, err, lager.Data{
-			"route": route,
-		})
+		routeLogger.Error(messages.FailedToPerformRequest, err)
 
 		return err
 	}
 
-	b, err := httputil.DumpResponse(res, true)
-	if err != nil {
+	if res.StatusCode >= 400 {
+		err = fmt.Errorf("HTTP bad response: %d", res.StatusCode)
+		routeLogger.Error("failed-to-ping-cloudcontroller", err)
 		return err
 	}
 
-	fmt.Fprintln(w, string(b))
+	// List Organizations
+	var organizations []OrganizationResource
+
+	route = cloudcontroller.ListOrganizations
+	routeLogger = routerLogger.WithData(lager.Data{
+		"route": route,
+	})
+	req, err = rg.CreateRequest(route, nil, nil)
+	if err != nil {
+		routeLogger.Error(messages.FailedToCreateRequest, err)
+		return err
+	}
+
+	res, err = client.Do(req)
+	if err != nil {
+		routeLogger.Error(messages.FailedToPerformRequest, err)
+		return err
+	}
+
+	defer res.Body.Close()
+
+	var listOrganizationsResponse ListOrganizationsResponse
+	err = json.NewDecoder(res.Body).Decode(&listOrganizationsResponse)
+	if err != nil {
+		routeLogger.Error("failed-to-decode-response", err)
+	}
+
+	organizations = append(organizations, listOrganizationsResponse.Resources...)
+
+	for _, organization := range organizations {
+		fmt.Fprintf(w, "%s: %s\n", organization.Metadata.GUID, organization.Entity.Name)
+	}
 
 	return nil
+}
+
+type ListOrganizationsResponse struct {
+	Resources []OrganizationResource `json:"resources"`
+}
+
+type OrganizationResource struct {
+	Metadata struct {
+		GUID string `json:"guid"`
+		URL  string `json:"url"`
+	} `json:"metadata"`
+	Entity struct {
+		Name               string `json:"name"`
+		SpacesURL          string `json:"spaces_url"`
+		UsersURL           string `json:"users_url"`
+		ManagersURL        string `json:"managers_url"`
+		BillingManagersURL string `json:"billing_managers_url"`
+		AuditorsURL        string `json:"auditors_url"`
+	} `json:"entity"`
 }
