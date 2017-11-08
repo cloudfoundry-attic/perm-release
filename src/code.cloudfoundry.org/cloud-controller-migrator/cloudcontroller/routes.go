db8ab0cbb9275e9f3d17347c9435b93f7764c1b7
Chris Hendrix <chendrix@pivotal.io>
2017-11-07 16:38:38 -0800

    Add cloudcontroller router
    [#151615847]

---
+++ src/code.cloudfoundry.org/cloud-controller-migrator/cloudcontroller/routes.go
@@ -0,0 +1,49 @@
+package cloudcontroller
+
+import (
+	"net/http"
+
+	"github.com/tedsuo/rata"
+)
+
+var Routes = rata.Routes{
+	{Name: Info, Path: "/v2/info", Method: rata.GET},
+
+	{Name: ListOrganizations, Path: "/v2/organizations", Method: rata.GET},
+	{Name: ListOrganizationSpaces, Path: "/v2/organizations/:guid/spaces", Method: rata.GET},
+	{Name: ListOrganizationAuditors, Path: "/v2/organizations/:guid/auditors", Method: rata.GET},
+	{Name: ListOrganizationBillingManagers, Path: "/v2/organizations/:guid/billing_managers", Method: rata.GET},
+	{Name: ListOrganizationManagers, Path: "/v2/organizations/:guid/managers", Method: rata.GET},
+	{Name: ListOrganizationUsers, Path: "/v2/organizations/:guid/users", Method: rata.GET},
+
+	{Name: ListSpaceAuditors, Path: "/v2/spaces/:guid/auditors", Method: rata.GET},
+	{Name: ListSpaceDevelopers, Path: "/v2/spaces/:guid/developers", Method: rata.GET},
+	{Name: ListSpaceManagers, Path: "/v2/spaces/:guid/managers", Method: rata.GET},
+}
+
+const (
+	Info = "info"
+
+	ListOrganizations = "list_organizations"
+
+	ListOrganizationSpaces = "list_organization_spaces"
+
+	ListOrganizationAuditors        = "list_organization_auditors"
+	ListOrganizationBillingManagers = "list_organization_billing_managers"
+	ListOrganizationManagers        = "list_organization_managers"
+	ListOrganizationUsers           = "list_organization_users"
+
+	ListSpaceAuditors   = "list_space_auditors"
+	ListSpaceDevelopers = "list_space_developers"
+	ListSpaceManagers   = "list_space_managers"
+)
+
+func NewRequestGenerator(host string) *rata.RequestGenerator {
+	rg := rata.NewRequestGenerator(host, Routes)
+
+	header := http.Header{}
+	header.Add("Accept", "application/json")
+
+	rg.Header = header
+	return rg
+}
--- src/code.cloudfoundry.org/cloud-controller-migrator/cmd/iterator.go
+++ src/code.cloudfoundry.org/cloud-controller-migrator/cmd/iterator.go
@@ -8,22 +8,17 @@ import (
 	"fmt"
 	"net/http/httputil"
 
-	"code.cloudfoundry.org/cloud-controller-migrator/httpx"
+	"code.cloudfoundry.org/cloud-controller-migrator/cloudcontroller"
 	"code.cloudfoundry.org/lager"
 )
 
 func IterateOverCloudControllerEntities(ctx context.Context, logger lager.Logger, w io.Writer, client *http.Client, url string) error {
-	infoURL, err := httpx.JoinURL(logger, url, "/v2/info")
+	rg := cloudcontroller.NewRequestGenerator(url)
+
+	req, err := rg.CreateRequest(cloudcontroller.Info, nil, nil)
 	if err != nil {
 		return err
 	}
-
-	req, err := http.NewRequest("GET", infoURL.String(), nil)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Accept", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
