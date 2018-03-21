551d25e10f6b9a5e52dd4c01934d88931fe82c1b
Slawek Ligus <sligus@pivotal.io>
2018-03-21 14:20:34 -0700

    refactor main test
    [#151620315]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- src/code.cloudfoundry.org/cc-to-perm-migrator/cmd/cc-to-perm-migrator/main_test.go
+++ src/code.cloudfoundry.org/cc-to-perm-migrator/cmd/cc-to-perm-migrator/main_test.go
@@ -16,193 +16,60 @@ import (
 	"github.com/onsi/gomega/ghttp"
 )
 
-type tokenJSON struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	RefreshToken string `json:"refresh_token"`
-	ExpiresIn    string `json:"expires_in"` // at least PayPal returns string, while most return number
-	Expires      string `json:"expires"`    // broken Facebook spelling of expires_in
-}
+const configTemplate = `log_level: info
 
-func appendHandler(server *ghttp.Server, method, path string, response interface{}) {
-	server.AppendHandlers(
-		ghttp.CombineHandlers(
-			ghttp.VerifyRequest(method, path, ""),
-			ghttp.RespondWithJSONEncoded(http.StatusOK, response),
-		),
-	)
-}
+uaa:
+  url: %s
+  ca_cert_path: /var/vcap/jobs/cc-to-perm-migrator/config/certs/uaa-ca.crt
+
+cloud_controller:
+  url: %s
+  client_id: perm-migrator
+  client_secret: secret
+  client_scopes: ["cloud_controller.admin_read_only"]
+`
 
 var _ = Describe("CCToPermMigrator", func() {
 	var (
-		server         *ghttp.Server
-		nextSpacesPage = "/v2/spaces/page2"
+		server *ghttp.Server
 
-		listSpacesResponse1Part1 capimodels.ListSpacesResponse
-		listSpacesResponse1Part2 capimodels.ListSpacesResponse
-		listSpacesResponse2      capimodels.ListSpacesResponse
-		listSpaceRolesNextPage   = "/v2/spaces/next"
+		orgsPage1 capimodels.ListOrgsResponse
+		orgsPage2 capimodels.ListOrgsResponse
 
-		listSpaceRolesResponse1Part1 capimodels.ListSpaceRolesResponse
-		listSpaceRolesResponse1Part2 capimodels.ListSpaceRolesResponse
-		listSpaceRolesResponse2      capimodels.ListSpaceRolesResponse
-		listSpaceRolesResponse3      capimodels.ListSpaceRolesResponse
-		nextOrgRolesPage             = "/v2/organizations/guid/next"
+		orgRoles1Page1   capimodels.ListOrgRolesResponse
+		orgRoles1Page2   capimodels.ListOrgRolesResponse
+		spaces1Page1     capimodels.ListSpacesResponse
+		spaces1Page2     capimodels.ListSpacesResponse
+		spaceRoles1Page1 capimodels.ListSpaceRolesResponse
+		spaceRoles1Page2 capimodels.ListSpaceRolesResponse
+		spaceRoles2      capimodels.ListSpaceRolesResponse
 
-		orgResponsePage1 capimodels.ListOrgsResponse
-		orgResponsePage2 capimodels.ListOrgsResponse
-		nextOrgsPage     = "/v2/organizations/page2"
+		orgRoles2   capimodels.ListOrgRolesResponse
+		spaces2     capimodels.ListSpacesResponse
+		spaceRoles3 capimodels.ListSpaceRolesResponse
 
-		listOrgRolesResponse1Part1 capimodels.ListOrgRolesResponse
-		listOrgRolesResponse1Part2 capimodels.ListOrgRolesResponse
-		listOrgRolesResponse2      capimodels.ListOrgRolesResponse
+		orgsNextURL       = "/v2/organizations/page2"
+		orgRolesNextURL   = "/v2/organizations/guid/next"
+		spacesNextURL     = "/v2/spaces/page2"
+		spaceRolesNextURL = "/v2/spaces/next"
 	)
 
 	BeforeEach(func() {
 		server = ghttp.NewServer()
 
-		listSpacesResponse1Part1 = capimodels.ListSpacesResponse{
-			PaginatedResponse: capimodels.PaginatedResponse{
-				NextURL: &nextSpacesPage,
-			},
-			Resources: []capimodels.SpaceResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "space-guid-1",
-					},
-				},
-			},
-		}
-		listSpacesResponse1Part2 = capimodels.ListSpacesResponse{
-			Resources: []capimodels.SpaceResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "space-guid-2",
-					},
-				},
-			},
-		}
-		listSpacesResponse2 = capimodels.ListSpacesResponse{
-			Resources: []capimodels.SpaceResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "space-guid-3",
-					},
-				},
-			},
-		}
+		orgsPage1 = createOrgResponse([]string{"org-guid-1"}, orgsNextURL)
+		orgsPage2 = createOrgResponse([]string{"org-guid-2"}, "")
+		orgRoles1Page1 = createOrgRolesResponse([]string{"test-user-1"}, orgRolesNextURL, []string{"org_developer"})
+		orgRoles1Page2 = createOrgRolesResponse([]string{"test-user-2"}, "", []string{"org_manager"})
+		spaces1Page1 = createSpacesResponse([]string{"space-guid-1"}, spacesNextURL)
+		spaces1Page2 = createSpacesResponse([]string{"space-guid-2"}, "")
+		spaceRoles1Page1 = createSpaceRolesResponse([]string{"test-user-1"}, spaceRolesNextURL, []string{"space_developer"})
+		spaceRoles1Page2 = createSpaceRolesResponse([]string{"test-user-2"}, "", []string{"space_manager"})
+		spaceRoles2 = createSpaceRolesResponse([]string{"test-user-3"}, "", []string{"space_developer"})
 
-		listSpaceRolesResponse1Part1 = capimodels.ListSpaceRolesResponse{
-			PaginatedResponse: capimodels.PaginatedResponse{
-				NextURL: &listSpaceRolesNextPage,
-			},
-			Resources: []capimodels.SpaceUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-1",
-					},
-					Entity: capimodels.SpaceUserResourceEntity{
-						Roles: []string{"space_developer"},
-					},
-				},
-			},
-		}
-		listSpaceRolesResponse1Part2 = capimodels.ListSpaceRolesResponse{
-			Resources: []capimodels.SpaceUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-3",
-					},
-					Entity: capimodels.SpaceUserResourceEntity{
-						Roles: []string{"space_manager"},
-					},
-				},
-			},
-		}
-		listSpaceRolesResponse2 = capimodels.ListSpaceRolesResponse{
-			Resources: []capimodels.SpaceUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-3",
-					},
-					Entity: capimodels.SpaceUserResourceEntity{
-						Roles: []string{"space_manager"},
-					},
-				},
-			},
-		}
-
-		listSpaceRolesResponse3 = capimodels.ListSpaceRolesResponse{
-			Resources: []capimodels.SpaceUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-3",
-					},
-					Entity: capimodels.SpaceUserResourceEntity{
-						Roles: []string{"space_auditor"},
-					},
-				},
-			},
-		}
-
-		orgResponsePage1 = capimodels.ListOrgsResponse{
-			PaginatedResponse: capimodels.PaginatedResponse{
-				NextURL: &nextOrgsPage,
-			},
-			Resources: []capimodels.OrgResource{{
-				Metadata: capimodels.MetadataResource{
-					GUID: "org-guid-1",
-				},
-			}},
-		}
-
-		orgResponsePage2 = capimodels.ListOrgsResponse{
-			Resources: []capimodels.OrgResource{{
-				Metadata: capimodels.MetadataResource{
-					GUID: "org-guid-2",
-				},
-			}},
-		}
-
-		listOrgRolesResponse1Part1 = capimodels.ListOrgRolesResponse{
-			PaginatedResponse: capimodels.PaginatedResponse{
-				NextURL: &nextOrgRolesPage,
-			},
-			Resources: []capimodels.OrgUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-1",
-					},
-					Entity: capimodels.OrgUserResourceEntity{
-						Roles: []string{"org_developer"},
-					},
-				},
-			},
-		}
-		listOrgRolesResponse1Part2 = capimodels.ListOrgRolesResponse{
-			Resources: []capimodels.OrgUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-1",
-					},
-					Entity: capimodels.OrgUserResourceEntity{
-						Roles: []string{"org_developer"},
-					},
-				},
-			},
-		}
-		listOrgRolesResponse2 = capimodels.ListOrgRolesResponse{
-			Resources: []capimodels.OrgUserResource{
-				{
-					Metadata: capimodels.MetadataResource{
-						GUID: "test-user-1",
-					},
-					Entity: capimodels.OrgUserResourceEntity{
-						Roles: []string{"org_developer"},
-					},
-				},
-			},
-		}
+		orgRoles2 = createOrgRolesResponse([]string{"test-user-1"}, "", []string{"billing_manager"})
+		spaces2 = createSpacesResponse([]string{"space-guid-3"}, "")
+		spaceRoles3 = createSpaceRolesResponse([]string{"test-user-1"}, "", []string{"space_auditor"})
 	})
 
 	AfterEach(func() {
@@ -215,43 +82,33 @@ var _ = Describe("CCToPermMigrator", func() {
 		var err error
 
 		BeforeEach(func() {
+			//These handlers are appended in the order in which they are called.
+			//If adding more handlers, make sure they are placed correctly in the set of calls.
 			appendHandler(server, "POST", "/oauth/token", tokenJSON{
 				AccessToken:  "cool",
 				TokenType:    "whatever",
 				RefreshToken: "something",
 				ExpiresIn:    "1234",
-				Expires:      "1234",
 			})
-			appendHandler(server, "GET", "/v2/organizations", orgResponsePage1)
-			appendHandler(server, "GET", nextOrgsPage, orgResponsePage2)
-			appendHandler(server, "GET", "/v2/organizations/org-guid-1/user_roles", listOrgRolesResponse1Part1)
-			appendHandler(server, "GET", nextOrgRolesPage, listOrgRolesResponse1Part2)
-			appendHandler(server, "GET", "/v2/organizations/org-guid-1/spaces", listSpacesResponse1Part1)
-			appendHandler(server, "GET", nextSpacesPage, listSpacesResponse1Part2)
-			appendHandler(server, "GET", "/v2/spaces/space-guid-1/user_roles", listSpaceRolesResponse1Part1)
-			appendHandler(server, "GET", listSpaceRolesNextPage, listSpaceRolesResponse1Part2)
+			appendHandler(server, "GET", "/v2/organizations", orgsPage1)
+			appendHandler(server, "GET", orgsNextURL, orgsPage2)
+			appendHandler(server, "GET", "/v2/organizations/org-guid-1/user_roles", orgRoles1Page1)
+			appendHandler(server, "GET", orgRolesNextURL, orgRoles1Page2)
+			appendHandler(server, "GET", "/v2/organizations/org-guid-1/spaces", spaces1Page1)
+			appendHandler(server, "GET", spacesNextURL, spaces1Page2)
+			appendHandler(server, "GET", "/v2/spaces/space-guid-1/user_roles", spaceRoles1Page1)
+			appendHandler(server, "GET", spaceRolesNextURL, spaceRoles1Page2)
 
-			appendHandler(server, "GET", "/v2/spaces/space-guid-2/user_roles", listSpaceRolesResponse2)
-			appendHandler(server, "GET", "/v2/organizations/org-guid-2/user_roles", listOrgRolesResponse2)
-			appendHandler(server, "GET", "/v2/organizations/org-guid-2/spaces", listSpacesResponse2)
-			appendHandler(server, "GET", "/v2/spaces/space-guid-3/user_roles", listSpaceRolesResponse3)
+			appendHandler(server, "GET", "/v2/spaces/space-guid-2/user_roles", spaceRoles2)
+			appendHandler(server, "GET", "/v2/organizations/org-guid-2/user_roles", orgRoles2)
+			appendHandler(server, "GET", "/v2/organizations/org-guid-2/spaces", spaces2)
+			appendHandler(server, "GET", "/v2/spaces/space-guid-3/user_roles", spaceRoles3)
 
 			tmpDir, err = ioutil.TempDir("", "ccmtest")
 			Expect(err).NotTo(HaveOccurred())
 			configFilePath = path.Join(tmpDir, "config.yml")
 
-			contents := fmt.Sprintf(`log_level: info
-
-uaa:
-  url: %s
-  ca_cert_path: /var/vcap/jobs/cc-to-perm-migrator/config/certs/uaa-ca.crt
-
-cloud_controller:
-  url: %s
-  client_id: perm-migrator
-  client_secret: secret
-  client_scopes: ["cloud_controller.admin_read_only"]
-`, server.URL(), server.URL())
+			contents := fmt.Sprintf(configTemplate, server.URL(), server.URL())
 			err = ioutil.WriteFile(configFilePath, []byte(contents), 0600)
 		})
 		It("exits with 1 when no flags are passed", func() {
@@ -260,3 +117,88 @@ cloud_controller:
 		})
 	})
 })
+
+func appendHandler(server *ghttp.Server, method, path string, response interface{}) {
+	server.AppendHandlers(
+		ghttp.CombineHandlers(
+			ghttp.VerifyRequest(method, path, ""),
+			ghttp.RespondWithJSONEncoded(http.StatusOK, response),
+		),
+	)
+}
+
+func createOrgResponse(GUIDs []string, nextPageURL string) capimodels.ListOrgsResponse {
+	response := capimodels.ListOrgsResponse{}
+	for _, guid := range GUIDs {
+		orgResource := capimodels.OrgResource{
+			Metadata: capimodels.MetadataResource{GUID: guid},
+		}
+		response.Resources = append(response.Resources, orgResource)
+	}
+	if nextPageURL != "" {
+		response.PaginatedResponse = capimodels.PaginatedResponse{
+			NextURL: &nextPageURL,
+		}
+	}
+	return response
+
+}
+
+func createOrgRolesResponse(GUIDs []string, nextPageURL string, roles []string) capimodels.ListOrgRolesResponse {
+	//Note: All resources are populated with the same roles
+	response := capimodels.ListOrgRolesResponse{}
+	for _, guid := range GUIDs {
+		orgUserResource := capimodels.OrgUserResource{
+			Metadata: capimodels.MetadataResource{GUID: guid},
+			Entity:   capimodels.OrgUserResourceEntity{Roles: roles},
+		}
+		response.Resources = append(response.Resources, orgUserResource)
+	}
+	if nextPageURL != "" {
+		response.PaginatedResponse = capimodels.PaginatedResponse{
+			NextURL: &nextPageURL,
+		}
+	}
+	return response
+}
+
+func createSpacesResponse(GUIDs []string, nextPageURL string) capimodels.ListSpacesResponse {
+	response := capimodels.ListSpacesResponse{}
+	for _, guid := range GUIDs {
+		spaceResource := capimodels.SpaceResource{
+			Metadata: capimodels.MetadataResource{GUID: guid},
+		}
+		response.Resources = append(response.Resources, spaceResource)
+	}
+	if nextPageURL != "" {
+		response.PaginatedResponse = capimodels.PaginatedResponse{
+			NextURL: &nextPageURL,
+		}
+	}
+	return response
+}
+
+func createSpaceRolesResponse(GUIDs []string, nextPageURL string, roles []string) capimodels.ListSpaceRolesResponse {
+	//Note: All resources are populated with the same roles
+	response := capimodels.ListSpaceRolesResponse{}
+	for _, guid := range GUIDs {
+		spaceUserResource := capimodels.SpaceUserResource{
+			Metadata: capimodels.MetadataResource{GUID: guid},
+			Entity:   capimodels.SpaceUserResourceEntity{Roles: roles},
+		}
+		response.Resources = append(response.Resources, spaceUserResource)
+	}
+	if nextPageURL != "" {
+		response.PaginatedResponse = capimodels.PaginatedResponse{
+			NextURL: &nextPageURL,
+		}
+	}
+	return response
+}
+
+type tokenJSON struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    string `json:"expires_in"`
+}
